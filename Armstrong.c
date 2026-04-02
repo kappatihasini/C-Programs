f0d7ab3ca86a59106f74e66df139590e9e13e341
Hasini.Reddy <kappatihasini9000@gmail.com>
2026-04-02 19:19:57 +0530

    Added basic C programs
    This commit includes basic C programs such as hello world, sum of two numbers, factorial, prime number, and armstrong number programs. These programs are part of my C programming practice.

---
+++ Armstrong.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, remainder, result = 0, original;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while(original != 0) {
+        remainder = original % 10;
+        result += remainder * remainder * remainder;
+        original /= 10;
+    }
+
+    if(result == n)
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+
+    return 0;
+}
